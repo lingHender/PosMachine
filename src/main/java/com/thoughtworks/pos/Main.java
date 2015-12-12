eeb4c24b96416ddabdfb3d3ead48b1ac69052427
linger <llf_hwj@163.com>
2015-12-12 11:39:07 +0800

    change main.java

--- src/main/java/com/thoughtworks/pos/Main.java
+++ src/main/java/com/thoughtworks/pos/Main.java
@@ -3,8 +3,10 @@ package com.thoughtworks.pos;
 import com.thoughtworks.pos.compute.DiscountPromotion;
 import com.thoughtworks.pos.compute.PromotionStrategy;
 import com.thoughtworks.pos.compute.SecondHalfPromotion;
+import com.thoughtworks.pos.compute.StrategyContext;
 import com.thoughtworks.pos.domain.BillItem;
 import com.thoughtworks.pos.domain.Item;
+import com.thoughtworks.pos.parser.FullReduceParser;
 import com.thoughtworks.pos.parser.ItemParser;
 import com.thoughtworks.pos.parser.BillItemParser;
 
@@ -14,10 +16,12 @@ import static java.util.Arrays.asList;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> billItems = asList("ITEM000001-3","ITEM000003-2","ITEM000005-3");
 
-        PosMachine posMachine = new PosMachine();
+        List<String> billItems = asList("ITEM000001-3","ITEM000003-5","ITEM000005-3");
 
-        System.out.println("总价:" + posMachine.calculateWithPromotion(billItems));
+        PosMachine posMachine = new PosMachine(new BillItemParser(),billItems);
+
+        System.out.println("总价:" + posMachine.calculate(new StrategyContext()));
+
     }
 }
